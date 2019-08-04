66e0028bddcc828a6ac2078d1b29c0dd4dd35c30
Willian-Ribeiro <willianribeirocorrea@gmail.com>
2019-08-04 05:24:10 -0300

    Organizando

--- Projeto Compilador/CompiladorANTLR/prog.cu
+++
@@ -1,9 +0,0 @@
-programa
-    declare a,b,c.
-    escreva("Meu primeiro programa em CU Soh CU eh universal").
-    leia( a ).
-    leia( b ).
-    c := a + b.
-    escreva("O resultado da soma vale").
-    escreva( c ).
-fimprog
--- Projeto-Compilador/README.md
+++
@@ -1,2 +0,0 @@
-# Compilador
-Desenvolvimento de um compilador para a matéria Compiladores
