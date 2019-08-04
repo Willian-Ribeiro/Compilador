b5a5713910f30b3d053b745b227b4eded2701470
Willian-Ribeiro <willianribeirocorrea@gmail.com>
2019-08-04 05:02:26 -0300

    remoção de msg teste e primeiro commit do trabalho

---
+++ Projeto Compilador/CompiladorANTLR/prog.cu
@@ -0,0 +1,9 @@
+programa
+    declare a,b,c.
+    escreva("Meu primeiro programa em CU Soh CU eh universal").
+    leia( a ).
+    leia( b ).
+    c := a + b.
+    escreva("O resultado da soma vale").
+    escreva( c ).
+fimprog
